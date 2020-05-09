edc41c9dbe592e46ac9636167cd205b8c79a1041
Davi Honorato <davii.silva64@gmail.com>
2020-05-09 09:28:28 -0300

    Alteração do readme e a modificação do arquivo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Jogo-do-acerto
- O computador irá gerar um número e pedirá para o usuário fazer tentativas e ver se consegue acertar.
+# Jogo_do_acerto
+ O computador gera um número e pede para o usuário fazer tentativas. Ao receber a entrada, analisa e responde conforme a resposta do usuário: se errou e precisa aumentar ou diminuir o valor ou se acertou. Ao final, imprime a quantidade de tentativas realizadas.
+ 
+ É um jogo origináriamente simples, utilizando apenas o prompt.
+ 
+ O objetivo desse projeto é criar um programa com comandos básicos do Python para desenvolver a lógica de programação e praticar o uso da sintaxe da linguagem.
---
+++ game/lib/__init__.py
@@ -0,0 +1,41 @@
+from random import randint
+
+
+def pensar():  # Irá gerar, para o 'Computer' um número aleatório entre 0 e 'x'.
+    n = randint(1, 3)
+    if n == 1:  # Fácil
+        num = randint(0, 10)
+        print('Pensei em um número entre 0 e 10. Tente acertar!')
+    elif n == 2:  # Intermediário
+        num = randint(0, 100)
+        print('Pensei em um número entre 0 e 100. Tente acertar!')
+    else:  # Difícil
+        num = randint(0, 1000)
+        print('Pensei em um número entre 0 e 1000. Tente acertar!')
+    return num
+
+
+def leiaInt(txt):  # Irá ler uma entrada do usuário e, dependendo, aceitar ou não como um número Inteiro.
+    n = str(input(txt))
+    if verificar(n):
+        num = int(n)
+        return num
+    else:
+        print('Digite um número inteiro válido.')
+
+
+def verificar(n):  # Verifica se um número indicado é um número Inteiro.
+    try:
+        num = int(n)  # Tenta tranformar o num é um número Inteiro
+    except:
+        return False
+    else:
+        return True
+
+
+def Aproximidade(player, computer):  # Irá indicar se será necessário digitar um número maior ou menor.
+    if player < computer:
+        print('Digite um número maior.')
+    else:
+        print('Digite um número menor.')
+
